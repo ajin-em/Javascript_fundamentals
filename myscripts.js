4917d963c585722ad76af4f92449dcc77795ccb5
ajin-em <ajinem2@gmail.com>
2024-05-23 10:08:59 +0530

    minor changes

--- myscripts.js
+++ myscripts.js
@@ -6,21 +6,22 @@
 // getName();
 // console.log(x);
 // console.log(getName);
-var x = 1;
-a();
-b();
-console.log(x);
+// var x = 1;
+// a();
+// b();
+// console.log(x);
 
-function a() {
-    var x = 10;
-    console.log(x);
-}
+// function a() {
+//     var x = 10;
+//     console.log(x);
+// }
 
-function b(){
-    var x = 100;
-    console.log(x);
-}
-
+// function b(){
+//     var x = 100;
+//     console.log(x);
+// }
+var a = 8;
+console.log(a)
 
 
  
