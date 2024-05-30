b688780685e4af16921ee9cf51aeba15585a78a3
ajin-em <ajinem2@gmail.com>
2024-05-30 23:16:11 +0530

    dom

--- myscripts.js
+++ myscripts.js
@@ -31,13 +31,16 @@
 
 // a()
 // <<=============================================================>>
-var a = 100
-{
-    var a = 10
-    let b = 20
-    const c = 30
-    console.log(a)
-    console.log(b)
-    console.log(c)
-}
+// var a = 100
+// {
+//     var a = 10
+//     let b = 20
+//     const c = 30
+//     console.log(a)
+//     console.log(b)
+//     console.log(c)
+// }
+// console.log(a)
+const a = 5
+a = 10
 console.log(a)
